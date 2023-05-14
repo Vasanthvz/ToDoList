2e9a1a7d781157edbca1f7d5f2218af96cbc058c
Vasanthvz <vasanthv1373@gmail.com>
2023-05-14 14:35:17 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Vasanth Kumar V on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
