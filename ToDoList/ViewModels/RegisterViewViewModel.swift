8d10a9bc13bd59e1cf7a108f9fc99a09c354b656
Vasanthvz <vasanthv1373@gmail.com>
2023-05-16 18:52:31 +0530

    changes

--- ToDoList/ViewModels/RegisterViewViewModel.swift
+++ ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Vasanth Kumar V on 14/05/23.
 //
-
+import FirebaseAuth
 import Foundation
 
 class RegisterViewViewModel:ObservableObject{
@@ -21,15 +21,30 @@ class RegisterViewViewModel:ObservableObject{
         guard validate() else{
             return
         }
+//        Back-ends
+        Auth.auth().createUser(withEmail: email, password: password){ [weak self] result ,error in
+            guard let userId = result?.user.uid else{
+                return
+            }
+            self?.insertUserRecord(i: userId)
+        }
     }
+    private func insertUserRecord(i:String){
+        
+    }
+    
     private func validate() -> Bool{
+        errorMessage = ""
         guard !name.trimmingCharacters(in: .whitespaces).isEmpty,!email.trimmingCharacters(in: .whitespaces).isEmpty,!password.trimmingCharacters(in: .whitespaces).isEmpty else{
+            errorMessage = "Fill all the Field"
             return false
         }
         guard email.contains("@") && email.contains(".") else{
+            errorMessage = "Enter a valid email"
             return false
         }
         guard password.count >= 6 else{
+            errorMessage = "enter password more than 6 characters"
             return false
         }
         return true
